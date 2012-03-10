6f4e268e2f0abb3916392614e363473fbb99faad
eagle.sakura <eagle.sakura@gmail.com>
2012-03-10 06:56:41 +0000

    レンダリング用のActivity/View/Dataを追加

---
+++ team-pants/PrettyIsPants/src/jp/preety/ispants/oekaki/data/Data.java
@@ -0,0 +1,54 @@
+package jp.preety.ispants.oekaki.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.eaglesakura.lib.android.game.math.Vector2;
+
+/**
+ * 描画用データクラス。
+ * データは基本的にJSON化を簡単にするため、全てpublicにする。
+ * @author TAKESHI YAMASHITA
+ *
+ */
+public class Data {
+    /**
+     * 描画用のペン
+     */
+    public Pen pen = null;
+
+    /**
+     * データを作成した時間。
+     * 書き込んだ時間が若い順から描画する。
+     */
+    public long beginTime = System.currentTimeMillis();
+
+    /**
+     * 描画用の点リスト
+     */
+    public List<Vector2> touchPoints = new ArrayList<Vector2>();
+
+    public Data() {
+
+    }
+
+    /**
+     * bluetoothやり取り用
+     * @param data
+     * @return
+     */
+    public static String toSendingData(Data data) {
+        //JSON 変換
+        return null;
+    }
+
+    /***
+     * bluetoothからの読み取り用
+     * @param data
+     * @return
+     */
+    public static Data fromRececiveData(String data) {
+        // JSONから変換
+        return null;
+    }
+}
---
+++ team-pants/PrettyIsPants/src/jp/preety/ispants/oekaki/data/Pen.java
@@ -0,0 +1,116 @@
+package jp.preety.ispants.oekaki.data;
+
+import com.eaglesakura.lib.android.game.graphics.Color;
+
+/**
+ * 
+ * @author TAKESHI YAMASHITA
+ *
+ */
+public class Pen {
+
+    /**
+     * 
+     * @author TAKESHI YAMASHITA
+     *
+     */
+    public enum Type {
+
+        /**
+         * 手書き
+         */
+        Tegaki,
+
+        /**
+         * スタンプ
+         */
+        Stamp,
+
+        /**
+         * フォトフレーム
+         */
+        Frame,
+
+        /**
+         * 美白レベル
+         */
+        Bihaku
+    }
+
+    /**
+     * 描画の種類設定
+     */
+    public Type type = Type.Tegaki;
+
+    /**
+     * ペンの幅
+     */
+    public float width = 5;
+
+    /**
+     * ペンの色
+     */
+    public Color color = new Color();
+
+    /**
+     * スタンプの名前
+     */
+    public String stampName = null;
+
+    /**
+     * フレームの名前
+     */
+    public String frameName = null;
+
+    /**
+     * 美白レベル
+     */
+    public float bihakuLevel = 0.0f;
+
+    /**
+     * 
+     */
+    public Pen() {
+
+    }
+
+    /**
+     * 手書きデータを指定する
+     * @param width ペン幅
+     * @param r 色R[0-255]
+     * @param g 色G[0-255]
+     * @param b 色B[0-255]
+     */
+    public void setTegakiData(float width, int r, int g, int b) {
+        this.width = width;
+        color.set(r, g, b, 255);
+        type = Type.Tegaki;
+    }
+
+    /**
+     * スタンプデータを設定する
+     * @param stampName
+     */
+    public void setStampData(String stampName) {
+        this.stampName = stampName;
+        type = Type.Stamp;
+    }
+
+    /**
+     * フォトフレームデータを設定する。
+     * @param frameName
+     */
+    public void setFrameData(String frameName) {
+        this.frameName = frameName;
+        type = Type.Frame;
+    }
+
+    /**
+     * 美白データを設定する。
+     * @param bihakuLevel
+     */
+    public void setBihakuData(float bihakuLevel) {
+        this.bihakuLevel = bihakuLevel;
+        type = Type.Bihaku;
+    }
+}
