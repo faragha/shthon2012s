35e7ae856e2592fbbf1f8e96598e453e59b794af
adakoda.jamjampan@gmail.com <adakoda.jamjampan@gmail.com@089b01dc-e5fd-bcf2-7e3d-aac3200c9482>
2012-03-10 17:49:38 +0000

    added save stamp image and share functions

--- trunk/the-b-team/android-app/sutami/src/jp/sonicstudio/sutami/image/PreviewView.java
+++ trunk/the-b-team/android-app/sutami/src/jp/sonicstudio/sutami/image/PreviewView.java
@@ -1,3 +1,4 @@
+
 package jp.sonicstudio.sutami.image;
 
 import android.content.Context;
@@ -8,55 +9,65 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+/**
+ * 画像プレビュービュークラス
+ */
 public class PreviewView extends View {
 
-	private String mImagePath;
-	private Bitmap mBitmap;
-	private int mViewWidth;
-	private int mViewHeight;
+    private Bitmap mBitmap;
+    private int mViewWidth;
+    private int mViewHeight;
 
-	public PreviewView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
+    public PreviewView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 
-	public PreviewView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public PreviewView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public PreviewView(Context context) {
-		super(context);
-	}
+    public PreviewView(Context context) {
+        super(context);
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		if (mBitmap != null) {
-			float scaleWidth = (float) mViewWidth / mBitmap.getWidth();
-			float scaleHeight = (float) mViewHeight / mBitmap.getHeight();
-			float scale = Math.min(scaleWidth, scaleHeight);
-			int scaledWidth = (int) (scale * mBitmap.getWidth());
-			int scaledHeight = (int) (scale * mBitmap.getHeight());
-			int offsetX = (mViewWidth - scaledWidth) / 2;
-			int offsetY = (mViewHeight - scaledHeight) / 2; // 上寄せ
-			Paint paint = new Paint();
-			Matrix matrix = new Matrix();
-			matrix.setScale(scale, scale);
-			canvas.translate(offsetX, offsetY);
-			canvas.drawBitmap(mBitmap, matrix, paint);
-		}
-	}
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mBitmap != null) {
+            // アスペクト比を保ってビューにフィットする拡大縮小率を計算
+            float scaleWidth = (float) mViewWidth / mBitmap.getWidth();
+            float scaleHeight = (float) mViewHeight / mBitmap.getHeight();
+            float scale = Math.min(scaleWidth, scaleHeight);
+            // ビュー内でセンタリングするためのオフセットを計算
+            int offsetX = (mViewWidth - (int) (scale * mBitmap.getWidth())) / 2;
+            int offsetY = (mViewHeight - (int) (scale * mBitmap.getHeight())) / 2;
+            // ビットマップを描画
+            Paint paint = new Paint();
+            Matrix matrix = new Matrix();
+            matrix.setScale(scale, scale);
+            canvas.save();
+            canvas.translate(offsetX, offsetY);
+            canvas.drawBitmap(mBitmap, matrix, paint);
+            canvas.restore();
+        }
+    }
 
-	public void setBitmap(Bitmap bitmap) {
-		mBitmap = bitmap;
-		invalidate();
-	}
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mViewWidth = w;
+        mViewHeight = h;
+        invalidate();
+    }
 
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		super.onSizeChanged(w, h, oldw, oldh);
-		mViewWidth = w;
-		mViewHeight = h;
-		invalidate();
-	}
+    /**
+     * ビューにビットマップを設定する
+     * 
+     * @param bitmap 表示したい画像
+     */
+    public void setBitmap(Bitmap bitmap) {
+        mBitmap = bitmap;
+        invalidate();
+    }
 
 }
