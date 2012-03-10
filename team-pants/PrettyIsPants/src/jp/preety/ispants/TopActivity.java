aaaa5e95551aa2a59cf5533298986b2931f4be5c
youtenredo <youtenredo@gmail.com>
2012-03-10 05:54:38 +0000

    [@youten_redo]Commit test

---
+++ team-pants/PrettyIsPants/src/jp/preety/ispants/TopActivity.java
@@ -0,0 +1,17 @@
+package jp.preety.ispants;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Top Activity
+ * @author pretty is pants
+ *
+ */
+public class TopActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
