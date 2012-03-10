131898927928aba8c098d66544828666165f932f
eagle.sakura <eagle.sakura@gmail.com>
2012-03-10 07:30:01 +0000

    Manifestの衝突を解消

--- team-pants/PrettyIsPants/src/jp/preety/ispants/oekaki/OekakiRender.java
+++ team-pants/PrettyIsPants/src/jp/preety/ispants/oekaki/OekakiRender.java
@@ -1,10 +1,14 @@
 package jp.preety.ispants.oekaki;
 
+import jp.preety.ispants.R;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
 import android.widget.FrameLayout;
 
 import com.eaglesakura.lib.android.game.graphics.gl11.OpenGLManager;
 import com.eaglesakura.lib.android.game.graphics.gl11.SpriteManager;
 import com.eaglesakura.lib.android.game.thread.AsyncHandler;
+import com.eaglesakura.lib.android.game.thread.ThreadSyncRunnerBase;
 import com.eaglesakura.lib.android.view.OpenGLView;
 
 /**
@@ -12,12 +16,32 @@ import com.eaglesakura.lib.android.view.OpenGLView;
  * @author TAKESHI YAMASHITA
  *
  */
-public class OekakiRender {
+public class OekakiRender implements Callback {
+
+    /**
+     * 
+     */
     OekakiActivity activity;
+
+    /**
+     * 
+     */
     OpenGLView glView = null;
+
+    /**
+     * 
+     */
     OpenGLManager glManager;
+
+    /**
+     * 描画用のスプライトマネージャ
+     */
     SpriteManager spriteManager;
-    AsyncHandler handler;
+
+    /**
+     * GL用レンダラ
+     */
+    AsyncHandler handler = AsyncHandler.createInstance();
 
     /**
      * 描画用のレイアウト
@@ -31,6 +55,13 @@ public class OekakiRender {
     public OekakiRender(OekakiActivity activity) {
         this.activity = activity;
 
+        {
+            layout = (FrameLayout) activity.findViewById(R.id.oekaki_area);
+            glView = new OpenGLView(activity);
+            glView.getHolder().addCallback(this);
+
+            layout.addView(glView);
+        }
     }
 
     /**
@@ -43,6 +74,43 @@ public class OekakiRender {
      * 
      */
     public void onResume() {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // 実行待ちを行う。
+        (new ThreadSyncRunnerBase<Void>(handler) {
+            @Override
+            public Void onOtherThreadRun() throws Exception {
+                if (activity.isFinishing()) {
+                    glManager.dispose();
+                } else {
+                    glManager.onPause();
+                }
+                return null;
+            }
+        }).run();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        glManager = glView.getGLManager();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (!glManager.isInitialized()) {
+                    glManager.initGL(handler);
+                } else {
+                    if (glView.isDestroyed()) {
+                        glManager.onResume();
+                    }
+                }
+            }
+        });
     }
 
     /**
