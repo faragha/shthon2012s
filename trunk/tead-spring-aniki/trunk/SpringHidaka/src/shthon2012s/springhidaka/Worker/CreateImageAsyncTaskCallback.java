ee06488c69bbd6dca7b7ca259ea3a7333ad2ec14
rabbitlog <rabbitlog@089b01dc-e5fd-bcf2-7e3d-aac3200c9482>
2012-03-10 19:06:02 +0000

    Webからの受け取りのAPIを追加。使い方はAsyncTaskSample.javaを見てー。

---
+++ trunk/tead-spring-aniki/trunk/SpringHidaka/src/shthon2012s/springhidaka/Worker/CreateImageAsyncTaskCallback.java
@@ -0,0 +1,27 @@
+package shthon2012s.springhidaka.Worker;
+
+/**
+ * ダウンロード＆画像生成完了通知用のコールバックインターフェイス
+ *
+ * @author mhidaka
+ *
+ */
+public interface CreateImageAsyncTaskCallback {
+
+   /**
+   * Webから画像を取得してフレーム合成が成功した時に呼ばれるメソッド
+   *
+   * @param filepath
+   *          作成画像の保存先のファイルパス
+   */
+  void onSuccessCreateImage(String filepath);
+
+  /**
+   * 画像のアップロードが失敗した時に呼ばれるメソッド
+   *
+   * @param statusCode
+   *          エラー時のステータスコード
+   *          (TODO:現状-1固定なのでネットワークがおかしいようです？とか出せば良いかも)
+   */
+  void onFailedCreateImage(int statusCode);
+}
