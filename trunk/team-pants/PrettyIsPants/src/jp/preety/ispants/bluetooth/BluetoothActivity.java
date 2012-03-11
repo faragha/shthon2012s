b98dcd05141a0e9d2d8088d8bdf940c72d051b88
dip.switch.et.al@gmail.com <dip.switch.et.al@gmail.com@089b01dc-e5fd-bcf2-7e3d-aac3200c9482>
2012-03-11 00:31:50 +0000

    onResumeでauto connect. only client mode

--- trunk/team-pants/PrettyIsPants/src/jp/preety/ispants/bluetooth/BluetoothActivity.java
+++ trunk/team-pants/PrettyIsPants/src/jp/preety/ispants/bluetooth/BluetoothActivity.java
@@ -1,3 +1,4 @@
+
 package jp.preety.ispants.bluetooth;
 
 import jp.preety.ispants.R;
@@ -13,36 +14,42 @@ import android.widget.Toast;
 
 public class BluetoothActivity extends Activity {
     private final String TAG = "BluetoothActivity";
-    
+
     // Intent request codes
     private static final int REQUEST_ENABLE_BT = 3;
+
     private static final int REQUEST_PICK_BLUETOOTH_DEVICE = 4;
+
     // Local Bluetooth adapter
     private BluetoothAdapter mBluetoothAdapter = null;
+
     // Member object for the chat services
     private BluetoothService mService = null;
+
     private int mServerFlag = 0;
-    
+
+    private boolean mBTClientSelected = false;
+
     String mConnectedDeviceName;
-    
+
     public static final String EXTRA_SERVER = "EXTRA_SERVER";
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter.equals(null)) {
             Toast.makeText(this, R.string.bluetooth_not_support, Toast.LENGTH_SHORT).show();
             finish();
         }
     }
-    
+
     @Override
     public void onStart() {
         super.onStart();
         Log.i(TAG, "++ ON START ++");
-        
+
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
@@ -62,121 +69,133 @@ public class BluetoothActivity extends Activity {
             }
         }
     }
-    
+
     // The Handler that gets information back from the BluetoothChatService
     private final Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-            case BluetoothService.MESSAGE_STATE_AS_SERVER_CHANGE:
-                if (mService != null && mService.getStateAsServer() == BluetoothService.STATE_NONE) {
-                    mService.startAcceptAsServer();
-                }
-                onChangeBluetoothServerMessageState();
-                break;
-            case BluetoothService.MESSAGE_STATE_AS_CLIENT_CHANGE:
-                Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
-                onChangeBluetoothClientMessageState();
-                break;
-            case BluetoothService.MESSAGE_READ:
-                byte[] readBuf = (byte[]) msg.obj;
-                // construct a string from the valid bytes in the buffer
-                String readMessage = new String(readBuf, 0, msg.arg1);
-                Log.d(TAG, readMessage);
-                sendToOtherDevice(readMessage, msg.arg2);
-                onMessageRead(readMessage);
-                break;
-            case BluetoothService.MESSAGE_DEVICE_NAME:
-                // save the connected device's name
-                mConnectedDeviceName = msg.getData().getString(BluetoothService.DEVICE_NAME);
-                onMessageDeviceName(mConnectedDeviceName);
-                break;
-            case BluetoothService.MESSAGE_TOAST:
-                Toast.makeText(getApplicationContext(), msg.getData().getString(BluetoothService.TOAST), Toast.LENGTH_SHORT).show();
-                break;
+                case BluetoothService.MESSAGE_STATE_AS_SERVER_CHANGE:
+                    if (mService != null
+                            && mService.getStateAsServer() == BluetoothService.STATE_NONE) {
+                        mService.startAcceptAsServer();
+                    }
+                    onChangeBluetoothServerMessageState();
+                    break;
+                case BluetoothService.MESSAGE_STATE_AS_CLIENT_CHANGE:
+                    Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                    onChangeBluetoothClientMessageState();
+                    break;
+                case BluetoothService.MESSAGE_READ:
+                    String readMessage = (String) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    // String readMessage = new String(readBuf, 0, msg.arg1);
+                    // Log.d(TAG, readMessage);
+                    sendToOtherDevice(readMessage, msg.arg1);
+                    onMessageRead(readMessage);
+                    break;
+                case BluetoothService.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(BluetoothService.DEVICE_NAME);
+                    onMessageDeviceName(mConnectedDeviceName);
+                    break;
+                case BluetoothService.MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(),
+                            msg.getData().getString(BluetoothService.TOAST), Toast.LENGTH_SHORT)
+                            .show();
+                    break;
             }
         }
     };
-    
-    protected void onChangeBluetoothServerMessageState(){};
-    
-    protected void onChangeBluetoothClientMessageState(){};
-    
-    protected void onMessageRead(String message){};
-    
-    protected void onMessageDeviceName(String connectedDeviceName){};
 
-    private void pickBluetooth() {
+    protected void onChangeBluetoothServerMessageState() {
+    };
+
+    protected void onChangeBluetoothClientMessageState() {
+    };
+
+    protected void onMessageRead(String message) {
+    };
+
+    protected void onMessageDeviceName(String connectedDeviceName) {
+    };
+
+    protected void pickBluetooth() {
         Intent intent = new Intent(this, BluetoothListPickerActivity.class);
         this.startActivityForResult(intent, REQUEST_PICK_BLUETOOTH_DEVICE);
     }
-    
+
     private void sendToOtherDevice(String sendData, int from) {
-        if(mService != null) {
-            switch(from) {
-            case BluetoothService.FROM_CLIENT:
-                if(mServerFlag == 0) {
-                    mService.writeAsClient(sendData.getBytes());
-                }
-                break;
-            case BluetoothService.FROM_SERVER:
-                mService.writeAsServer(sendData.getBytes());
-                break;
-            default:
-                mService.writeAsServer(sendData.getBytes());
-                if(mServerFlag == 0) {
-                    mService.writeAsClient(sendData.getBytes());
-                }
-                break;
+        if (mService != null) {
+            switch (from) {
+                case BluetoothService.FROM_CLIENT:
+                    if (mServerFlag == 0) {
+                        mService.writeAsClient(sendData);
+                    }
+                    break;
+                case BluetoothService.FROM_SERVER:
+                    mService.writeAsServer(sendData);
+                    break;
+                default:
+                    mService.writeAsServer(sendData);
+                    if (mServerFlag == 0) {
+                        mService.writeAsClient(sendData);
+                    }
+                    break;
             }
         }
     }
-    
+
     protected void sendToOtherDevice(String sendData) {
         sendToOtherDevice(sendData, -1);
     }
-    
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.d(TAG, "onActivityResult " + resultCode);
         switch (requestCode) {
-        case REQUEST_ENABLE_BT:
-            // When the request to enable Bluetooth returns
-            if (resultCode == Activity.RESULT_OK) {
-                mService = new BluetoothService(this, mHandler);
-                mService.start();
-            } else {
-                // User did not enable Bluetooth or an error occurred
-                Log.d(TAG, "BT not enabled");
-                Toast.makeText(this, R.string.bluetooth_not_enabled, Toast.LENGTH_SHORT).show();
-                finish();
-            }
-            break;
-        case REQUEST_PICK_BLUETOOTH_DEVICE:
-            if (resultCode == Activity.RESULT_OK) {
-                Toast.makeText(this, R.string.connecting, Toast.LENGTH_SHORT).show();
-                connectDevice(data.getExtras().getString("address"));
-            }
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    mService = new BluetoothService(this, mHandler);
+                    mService.start();
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    Log.d(TAG, "BT not enabled");
+                    Toast.makeText(this, R.string.bluetooth_not_enabled, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            case REQUEST_PICK_BLUETOOTH_DEVICE:
+                mBTClientSelected = false;
+                if (resultCode == Activity.RESULT_OK) {
+                    Toast.makeText(this, R.string.connecting, Toast.LENGTH_SHORT).show();
+                    connectDevice(data.getExtras().getString("address"));
+                    mBTClientSelected = true;
+                }
         }
     }
-    
+
     private void connectDevice(String address) {
         BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
         mService.connectAsClient(device);
     }
-    
+
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "onPause");
     }
-    
+
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "onResume");
+        if (mServerFlag == 0 && !mBTClientSelected) {
+            pickBluetooth();
+        }
     }
-    
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -187,4 +206,3 @@ public class BluetoothActivity extends Activity {
         }
     }
 }
-
