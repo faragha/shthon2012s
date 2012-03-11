7111ccdb5b508eee2212f4cab35b9704d05be85e
rabbitlog <rabbitlog@089b01dc-e5fd-bcf2-7e3d-aac3200c9482>
2012-03-11 04:34:13 +0000

    春のアニキのベータ版

--- trunk/tead-spring-aniki/trunk/SpringHidaka/src/shthon2012s/springhidaka/ActPicture.java
+++ trunk/tead-spring-aniki/trunk/SpringHidaka/src/shthon2012s/springhidaka/ActPicture.java
@@ -3,20 +3,30 @@ package shthon2012s.springhidaka;
 import java.io.FileOutputStream;
 
 import shthon2012s.springhidaka.Utils.Utils;
+import shthon2012s.springhidaka.WebApis.UploadAsyncTask;
+import shthon2012s.springhidaka.Worker.CreateImageAsyncTask;
+import shthon2012s.springhidaka.Worker.CreateImageAsyncTaskCallback;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ActPicture extends Activity {
+public class ActPicture extends Activity implements CreateImageAsyncTaskCallback {
 
 	private Context ctx;
 	Bitmap bm;
+
+    public Handler h;
+    private Runnable r;
+    private static String mFilepath;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,25 +35,52 @@ public class ActPicture extends Activity {
 
 		ctx = this.getApplicationContext();
 
-		// ためしにかさねてみたよ
-		String imagePath = this.getIntent().getStringExtra("filepath");
-		Overlay(imagePath);
+		if(Intent.ACTION_VIEW.equals(getIntent().getAction())){
 
-		Boolean isSave = this.getIntent().getBooleanExtra("saveflag", false);
+			Utils.createDrawbleCollection();
+			String imageUrl = getIntent().getDataString();
+			//データ生成
+    		CreateImageAsyncTask get = new CreateImageAsyncTask(this, this);
+    		get.execute( imageUrl );
+		}else{
 
-		if (isSave) {
-			try {
-				FileOutputStream out = openFileOutput(imagePath,
-						Context.MODE_WORLD_READABLE);
-				bm.compress(Bitmap.CompressFormat.JPEG, 100, out);
-				out.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+			// 画像のフルパスを取得するよ
+			String imagePath = this.getIntent().getStringExtra("filepath");
+			Overlay(imagePath);
+
+			// 保存する場合はセーブフラグを用意
+			Boolean isSave = this.getIntent().getBooleanExtra("saveflag", false);
+
+			if (isSave) {
+				saveImage(imagePath);
 			}
 		}
 
+		mFilepath = new String();
+	    h = new Handler();
+	    r = new Runnable() {
+	        @Override
+	        public void run() {
+
+	    		Overlay(mFilepath);
+	    		saveImage(mFilepath);
+	        }
+	    };
+
+
 		//どっかきめたフォルダに保存したの読んでgridviewとか？
 		//ファイル名を時刻＋セリフにしちゃえば管理楽なんじゃね
+	}
+
+	private void saveImage(String imagePath) {
+		try {
+			FileOutputStream out = openFileOutput(imagePath,
+					Context.MODE_WORLD_READABLE);
+			bm.compress(Bitmap.CompressFormat.JPEG, 100, out);
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void Overlay(String imagePath) {
@@ -86,5 +123,21 @@ public class ActPicture extends Activity {
 		ImageView image = (ImageView) findViewById(R.id.imageView1);
 		image.setImageBitmap(bm);
 
+	}
+
+
+
+
+	@Override
+	public void onSuccessCreateImage(String filepath) {
+
+		mFilepath = filepath;
+		h.post(r);
+	}
+
+	@Override
+	public void onFailedCreateImage(int statusCode) {
+
+		Toast.makeText(getApplicationContext(), "ごめんね。失敗しちゃった…" + Integer.toString(statusCode), Toast.LENGTH_SHORT).show();
 	}
 }
--- trunk/tead-spring-aniki/trunk/SpringHidaka/src/shthon2012s/springhidaka/ActSplash.java
+++ trunk/tead-spring-aniki/trunk/SpringHidaka/src/shthon2012s/springhidaka/ActSplash.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 public class ActSplash extends Activity {
 
