5c430c12f27e55bf2ac003da4282729dfacaf55f
shogo.oogle@gmail.com <shogo.oogle@gmail.com@089b01dc-e5fd-bcf2-7e3d-aac3200c9482>
2012-03-11 00:43:40 +0000

    完了画面を追加

---
+++ trunk/team-pants/PrettyIsPants/src/jp/preety/ispants/CompleteActivity.java
@@ -0,0 +1,40 @@
+package jp.preety.ispants;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Complete Activity
+ * @author pretty is pants
+ */
+public class CompleteActivity extends Activity implements View.OnClickListener {
+    /**
+     * 画像の保存先を伝える
+     */
+    public static final String INTENT_IMAGE_URI = "INTENT_IMAGE_URI";
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirm);
+        
+        // ボタンイベント登録
+        final Button btnOk = (Button)findViewById(R.id.btn_ok);
+        btnOk.setOnClickListener(this);
+    }
+    
+    /**
+     * Event Hanlder: on click view
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_ok: {
+                finish();
+                break;
+            }
+        }
+    }
+}
