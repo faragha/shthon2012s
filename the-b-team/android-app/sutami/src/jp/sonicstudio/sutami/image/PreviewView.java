1da3a821074b14d050c9f76d0f9dafdf678d6b4e
adakoda.jamjampan <adakoda.jamjampan@gmail.com>
2012-03-13 13:32:31 +0000

    fixed for "is never read locally" warning

--- the-b-team/android-app/sutami/src/jp/sonicstudio/sutami/image/PreviewView.java
+++ the-b-team/android-app/sutami/src/jp/sonicstudio/sutami/image/PreviewView.java
@@ -11,7 +11,6 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.Paint.Style;
-import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
