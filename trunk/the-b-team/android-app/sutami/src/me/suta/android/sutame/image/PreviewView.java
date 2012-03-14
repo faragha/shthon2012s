21ef7cacb880059f6638e55c63315a1a2e6c26e1
miyaken5 <miyaken5@089b01dc-e5fd-bcf2-7e3d-aac3200c9482>
2012-03-14 06:39:20 +0000

    ログ一部削除

--- trunk/the-b-team/android-app/sutami/src/me/suta/android/sutame/image/PreviewView.java
+++ trunk/the-b-team/android-app/sutami/src/me/suta/android/sutame/image/PreviewView.java
@@ -24,32 +24,30 @@ public class PreviewView extends View {
     private static final int RECT_LINE_SIZE = 5;
     private static final String COLOR_RECT_LINE = "#E100E5";
     private static final int SELECT_LINE_MARGE = RECT_LINE_SIZE * 2;
-    
+
     private Bitmap mBitmap;
     private int mViewWidth;
     private int mViewHeight;
     private Bitmap mCloseImage;
     private boolean mShowRectFlag = false;
-    private RectF mRectf = new RectF(0, 0, 0, 0) ;
-    
-
+    private RectF mRectf = new RectF(0, 0, 0, 0);
 
     public PreviewView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        
-        mCloseImage = BitmapFactory.decodeResource(getResources(),R.drawable.close);
+
+        mCloseImage = BitmapFactory.decodeResource(getResources(), R.drawable.close);
     }
 
     public PreviewView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        
-        mCloseImage = BitmapFactory.decodeResource(getResources(),R.drawable.close);
+
+        mCloseImage = BitmapFactory.decodeResource(getResources(), R.drawable.close);
     }
 
     public PreviewView(Context context) {
         super(context);
-        
-        mCloseImage = BitmapFactory.decodeResource(getResources(),R.drawable.close);
+
+        mCloseImage = BitmapFactory.decodeResource(getResources(), R.drawable.close);
     }
 
     @Override
@@ -67,14 +65,14 @@ public class PreviewView extends View {
             Paint paint = new Paint();
             Matrix matrix = new Matrix();
             matrix.setScale(scale, scale);
-            
+
             canvas.save();
             canvas.translate(offsetX, offsetY);
             canvas.drawBitmap(mBitmap, matrix, paint);
             canvas.restore();
-            
+
             drawSelectRect(canvas);
-            
+
         }
     }
 
@@ -96,226 +94,215 @@ public class PreviewView extends View {
         invalidate();
     }
 
-    
     private float closeImageX = 0;
     private float closeImageY = 0;
-    
 
-    
-    public void drawSelectRect(Canvas canvas){
-	
-	if(mRectf.left != 0 || mRectf.top != 0 || mRectf.right !=0 || mRectf.bottom != 0){
-	    Paint paint = new Paint();
-	    paint.setColor(Color.parseColor(COLOR_RECT_LINE)); 
-	    paint.setStrokeWidth(RECT_LINE_SIZE);
-	    paint.setStyle(Style.STROKE);
-	    canvas.drawRect(mRectf, paint);
-	}
-	
-	if (mShowRectFlag) {
-	    Paint closePaint = new Paint();
-	    closeImageX = mRectf.right - mCloseImage.getWidth() / 2;
-	    closeImageY = mRectf.top - mCloseImage.getHeight() + mCloseImage.getHeight() / 2;
+    public void drawSelectRect(Canvas canvas) {
 
-	    if (closeImageX > getWidth()) {
-		closeImageX = getWidth() - mCloseImage.getWidth() - RECT_LINE_SIZE - 20;
-	    }
+        if (mRectf.left != 0 || mRectf.top != 0 || mRectf.right != 0 || mRectf.bottom != 0) {
+            Paint paint = new Paint();
+            paint.setColor(Color.parseColor(COLOR_RECT_LINE));
+            paint.setStrokeWidth(RECT_LINE_SIZE);
+            paint.setStyle(Style.STROKE);
+            canvas.drawRect(mRectf, paint);
+        }
 
-	    canvas.drawBitmap(mCloseImage, closeImageX, closeImageY, closePaint);
-	}
+        if (mShowRectFlag) {
+            Paint closePaint = new Paint();
+            closeImageX = mRectf.right - mCloseImage.getWidth() / 2;
+            closeImageY = mRectf.top - mCloseImage.getHeight() + mCloseImage.getHeight() / 2;
+
+            if (closeImageX > getWidth()) {
+                closeImageX = getWidth() - mCloseImage.getWidth() - RECT_LINE_SIZE - 20;
+            }
+
+            canvas.drawBitmap(mCloseImage, closeImageX, closeImageY, closePaint);
+        }
     }
-    
+
     private static final int SELECT_LINE_LEFT = 1;
     private static final int SELECT_LINE_TOP = 2;
     private static final int SELECT_LINE_RIGHT = 3;
     private static final int SELECT_LINE_BOTTOM = 4;
     private int mSelectLineNum = 0;
-    
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-	switch (event.getAction()) {
-	    case MotionEvent.ACTION_DOWN:
-		Log.d("me","ACTION_DOWN");
-		if(!mShowRectFlag){
-		    // 選択の枠始点設定
-		    mRectf.left = event.getX();
-		    mRectf.top = event.getY();
-		    mRectf.right = event.getX();
-		    mRectf.bottom = event.getY();
-		}else{
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (!mShowRectFlag) {
+                    // 選択の枠始点設定
+                    mRectf.left = event.getX();
+                    mRectf.top = event.getY();
+                    mRectf.right = event.getX();
+                    mRectf.bottom = event.getY();
+                } else {
 
-		    float x =  event.getX();
-		    float y = event.getY();
+                    float x = event.getX();
+                    float y = event.getY();
 
-		    //選択された線を記憶
-		    if(isSelectLineLeft(x)){
-			mSelectLineNum = SELECT_LINE_LEFT;
-		    }else 
-		    
-		    if(isSelectLineRight(x)){
-			mSelectLineNum = SELECT_LINE_RIGHT;
-		    }else		    
-		    
-		    if(isSelectTop(y)){
-			mSelectLineNum = SELECT_LINE_TOP;
-		    }else	
-	
-		    if(isSelectBottom(y)){
-			mSelectLineNum = SELECT_LINE_BOTTOM; 
-		    }
-		}
-		
-		
-	        break;
-	    case MotionEvent.ACTION_UP:
-		if(!mShowRectFlag){
-		    
-		    Log.d("me","ACTION_UP");
-		    // 選択の枠終点指定
-		    mRectf.right = event.getX();
-		    mRectf.bottom = event.getY();
-		    
-		    mShowRectFlag = true;
-		    exchangePoints();
-		    invalidate();
-		}else{
-		    float  x= event.getX();
-		    float y =  event.getY();
-		    if(closeImageX < x && x < closeImageX + mCloseImage.getWidth() && closeImageY < y && y < closeImageY + mCloseImage.getHeight()){
-			// 閉じるボタンを押した
-			mRectf.set(0, 0, 0, 0);
-			mShowRectFlag = false;
-			invalidate();
-		    }else{
-			setSelectLineMovePoint(event);
-			exchangePoints();
-			mSelectLineNum = 0;
-		    }
-		}
-		    
-		
-		
-	        break;
-	    case MotionEvent.ACTION_MOVE:
-		Log.d("me","ACTION_MOVE");
-		if(!mShowRectFlag){
-		    // 選択の枠終点指定
-		    mRectf.right = event.getX();
-		    mRectf.bottom = event.getY();
-		    
-		}else{
-		    setSelectLineMovePoint(event);
-		}
-		invalidate();
-	        break;
-	    }
-	
-	return true;
+                    // 選択された線を記憶
+                    if (isSelectLineLeft(x)) {
+                        mSelectLineNum = SELECT_LINE_LEFT;
+                    } else
+
+                    if (isSelectLineRight(x)) {
+                        mSelectLineNum = SELECT_LINE_RIGHT;
+                    } else
+
+                    if (isSelectTop(y)) {
+                        mSelectLineNum = SELECT_LINE_TOP;
+                    } else
+
+                    if (isSelectBottom(y)) {
+                        mSelectLineNum = SELECT_LINE_BOTTOM;
+                    }
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+                if (!mShowRectFlag) {
+
+                    // 選択の枠終点指定
+                    mRectf.right = event.getX();
+                    mRectf.bottom = event.getY();
+
+                    mShowRectFlag = true;
+                    exchangePoints();
+                    invalidate();
+                } else {
+                    float x = event.getX();
+                    float y = event.getY();
+                    if (closeImageX < x && x < closeImageX + mCloseImage.getWidth()
+                            && closeImageY < y && y < closeImageY + mCloseImage.getHeight()) {
+                        // 閉じるボタンを押した
+                        mRectf.set(0, 0, 0, 0);
+                        mShowRectFlag = false;
+                        invalidate();
+                    } else {
+                        setSelectLineMovePoint(event);
+                        exchangePoints();
+                        mSelectLineNum = 0;
+                    }
+                }
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                if (!mShowRectFlag) {
+                    // 選択の枠終点指定
+                    mRectf.right = event.getX();
+                    mRectf.bottom = event.getY();
+
+                } else {
+                    setSelectLineMovePoint(event);
+                }
+                invalidate();
+                break;
+        }
+
+        return true;
     }
 
     private void setSelectLineMovePoint(MotionEvent event) {
-	switch (mSelectLineNum) {
-	case SELECT_LINE_LEFT:
-	    mRectf.left = event.getX();
-	    
-	    invalidate();
-	    break;
+        switch (mSelectLineNum) {
+            case SELECT_LINE_LEFT:
+                mRectf.left = event.getX();
 
-	case SELECT_LINE_TOP:
-	    mRectf.top = event.getY();
-	    
-	    invalidate();
-	    break;
+                invalidate();
+                break;
 
-	case SELECT_LINE_RIGHT:
-	    mRectf.right = event.getX();
-	    
-	    invalidate();
-	    break;
+            case SELECT_LINE_TOP:
+                mRectf.top = event.getY();
 
-	case SELECT_LINE_BOTTOM:
-	    mRectf.bottom = event.getY();
-	    invalidate();
-	    break;
+                invalidate();
+                break;
 
-	    
-	default:
-	    break;
-	}
+            case SELECT_LINE_RIGHT:
+                mRectf.right = event.getX();
+
+                invalidate();
+                break;
+
+            case SELECT_LINE_BOTTOM:
+                mRectf.bottom = event.getY();
+                invalidate();
+                break;
+
+            default:
+                break;
+        }
     }
-    
-    private void exchangePoints(){
-	
-	
-	if(mRectf.right < mRectf.left){
-	    float tmp = mRectf.left;
-	    mRectf.left = mRectf.right;
-	    mRectf.right = tmp;
-	}
-	if(mRectf.bottom < mRectf.top){
-	    float tmp = mRectf.top;
-	    mRectf.top = mRectf.bottom;
-	    mRectf.bottom = tmp;
-	}
-	if(mRectf.left < RECT_LINE_SIZE){
-	    mRectf.left = RECT_LINE_SIZE;
-	}
-	if(mRectf.top < RECT_LINE_SIZE){
-	    mRectf.top = RECT_LINE_SIZE;
-	}
-	if(mRectf.right > getWidth() -RECT_LINE_SIZE){
-	    mRectf.right =  getWidth() -RECT_LINE_SIZE;
-	}
-	if(mRectf.bottom > getHeight() -RECT_LINE_SIZE){
-	    mRectf.bottom =  getHeight() -RECT_LINE_SIZE;
-	}
-	
-	
+
+    private void exchangePoints() {
+
+        if (mRectf.right < mRectf.left) {
+            float tmp = mRectf.left;
+            mRectf.left = mRectf.right;
+            mRectf.right = tmp;
+        }
+        if (mRectf.bottom < mRectf.top) {
+            float tmp = mRectf.top;
+            mRectf.top = mRectf.bottom;
+            mRectf.bottom = tmp;
+        }
+        if (mRectf.left < RECT_LINE_SIZE) {
+            mRectf.left = RECT_LINE_SIZE;
+        }
+        if (mRectf.top < RECT_LINE_SIZE) {
+            mRectf.top = RECT_LINE_SIZE;
+        }
+        if (mRectf.right > getWidth() - RECT_LINE_SIZE) {
+            mRectf.right = getWidth() - RECT_LINE_SIZE;
+        }
+        if (mRectf.bottom > getHeight() - RECT_LINE_SIZE) {
+            mRectf.bottom = getHeight() - RECT_LINE_SIZE;
+        }
+
     }
 
     private boolean isSelectLineLeft(float x) {
-	if (mRectf.left - SELECT_LINE_MARGE <= x && x <= mRectf.left + SELECT_LINE_MARGE) {
-	    return true;
-	}
-	return false;
+        if (mRectf.left - SELECT_LINE_MARGE <= x && x <= mRectf.left + SELECT_LINE_MARGE) {
+            return true;
+        }
+        return false;
     }
 
     private boolean isSelectLineRight(float x) {
-	if (mRectf.right - SELECT_LINE_MARGE <= x && x <= mRectf.right + SELECT_LINE_MARGE) {
-	    return true;
-	}
+        if (mRectf.right - SELECT_LINE_MARGE <= x && x <= mRectf.right + SELECT_LINE_MARGE) {
+            return true;
+        }
 
-	return false;
+        return false;
     }
 
     private boolean isSelectTop(float y) {
-	if (mRectf.top - SELECT_LINE_MARGE <= y && y <= mRectf.top + SELECT_LINE_MARGE) {
-	    return true;
-	}
-	return false;
+        if (mRectf.top - SELECT_LINE_MARGE <= y && y <= mRectf.top + SELECT_LINE_MARGE) {
+            return true;
+        }
+        return false;
     }
 
     private boolean isSelectBottom(float y) {
-	if (mRectf.bottom - SELECT_LINE_MARGE <= y && y <= mRectf.bottom + SELECT_LINE_MARGE) {
-	    return true;
-	}
-	return false;
+        if (mRectf.bottom - SELECT_LINE_MARGE <= y && y <= mRectf.bottom + SELECT_LINE_MARGE) {
+            return true;
+        }
+        return false;
     }
-    
-    
+
     // 画像の最大の長さ
     private static final int IMAGE_MAX_LENGTH = 140;
-    
-    public Bitmap getSelectImage(){
-	
-	Bitmap outputBitmap;
-	
-	if(!mShowRectFlag){
-	    outputBitmap = mBitmap;
-	}else{
 
-	    // TODO 以下の内容はコピーが含まれているので後で見直し。
+    public Bitmap getSelectImage() {
+
+        Bitmap outputBitmap;
+
+        if (!mShowRectFlag) {
+            outputBitmap = mBitmap;
+        } else {
+
+            // TODO 以下の内容はコピーが含まれているので後で見直し。
 
             Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
             Canvas canvas = new Canvas(bitmap);
@@ -338,24 +325,25 @@ public class PreviewView extends View {
 
             outputBitmap = Bitmap.createBitmap(bitmap, (int) mRectf.left, (int) mRectf.top,
                     (int) mRectf.width(), (int) mRectf.height());
-	}
-	
-	// Bitmap画像のサイズ縮小
-	    int width = outputBitmap.getWidth();
-	    int height = outputBitmap.getHeight();
-	    
+        }
+
+        // Bitmap画像のサイズ縮小
+        int width = outputBitmap.getWidth();
+        int height = outputBitmap.getHeight();
+
         float scale = 1;
         if (width > height) {
-            scale = (float)IMAGE_MAX_LENGTH / width;
+            scale = (float) IMAGE_MAX_LENGTH / width;
         } else {
-            scale = (float)IMAGE_MAX_LENGTH / height;
+            scale = (float) IMAGE_MAX_LENGTH / height;
         }
-        
-        if(scale > 1){
+
+        if (scale > 1) {
             scale = 1;
         }
 
-	    return Bitmap.createScaledBitmap(outputBitmap, (int)(width*scale), (int)(height*scale), false);
+        return Bitmap.createScaledBitmap(outputBitmap, (int) (width * scale),
+                (int) (height * scale), false);
     }
-    
+
 }
